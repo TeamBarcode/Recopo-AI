adf714ccea18eb70c88ad059598e23e46c0c083e
hanseo <hankim0421@gmail.com>
2026-07-23 18:47:24 +0900

    chore:  설정값 추가

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,3 @@
-#.env 값을 코드에서 쓸 수 있게 불러오는 파일
 import os
 from dotenv import load_dotenv
 
@@ -8,7 +7,17 @@ load_dotenv()
 class Settings:
     GITHUB_TOKEN: str = os.getenv("GITHUB_TOKEN", "")
     GITHUB_API_URL: str = os.getenv("GITHUB_API_URL", "https://api.github.com")
-    GITHUB_TIMEOUT: int = 10
+    GITHUB_TIMEOUT: int = int(os.getenv("GITHUB_TIMEOUT", "10"))
+
+    GITHUB_SEARCH_PER_PAGE: int = int(os.getenv("GITHUB_SEARCH_PER_PAGE", "30"))
+    GITHUB_README_CANDIDATE_LIMIT: int = int(
+        os.getenv("GITHUB_README_CANDIDATE_LIMIT", "10")
+    )
+    GITHUB_README_MAX_CHARS: int = int(
+        os.getenv("GITHUB_README_MAX_CHARS", "4000")
+    )
+
+    MIN_REPOSITORY_STARS: int = int(os.getenv("MIN_REPOSITORY_STARS", "5"))
 
 
 settings = Settings()
